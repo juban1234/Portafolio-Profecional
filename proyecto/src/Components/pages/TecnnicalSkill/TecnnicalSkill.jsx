4ba98dd3e7f27ca0b06e2c303c81d5de5c1a0832
juan-david-restrepo <davyz2006@gmail.com>
2025-03-07 14:11:52 -0500

    creacion de la pagina sobre las tecnicas aprendidas

--- proyecto/src/Components/pages/TecnnicalSkill/TecnnicalSkill.jsx
+++ proyecto/src/Components/pages/TecnnicalSkill/TecnnicalSkill.jsx
@@ -3,7 +3,15 @@
 export const TecnnicalSkill = () => {
   return (
     <>
-        <h1>como estas</h1>
+        <main>
+            <h1> conocimientos tecnicos</h1>
+            <p>
+            - **Frontend:** React, HTML, CSS, JavaScript, Tailwind CSS.
+            - **Control de Versiones:** Git, GitHub.
+            - **Educación y Liderazgo:** Lógica de programación, preparación y conducción de conferencias.
+            - **Desarrollo Web**
+            </p>
+        </main>
     </>
   )
 }
