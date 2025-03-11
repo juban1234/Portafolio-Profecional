f900c4adf659fe169cc6c67bf8d07aaf4a79df0f
juan <alexzo8677@gmail.com>
2025-03-11 17:43:24 -0500

    actualizacion

--- proyecto/src/Components/pages/ProfessionalExp/ProfessionalExp.jsx
+++ proyecto/src/Components/pages/ProfessionalExp/ProfessionalExp.jsx
@@ -3,6 +3,8 @@ export const ProfessionalExp = () => {
   return (
     <main className="flex-grow min-h-screen flex flex-col items-center p-4">
       <h1>experiencia profecional</h1>
+      <p>no cuento con experiencia laboral pero no letengo miedo al trabajo</p>
+      
     </main>
   )
 }
