7994c6802f104ed4a86641f50f6ae34272b63288
juan <alexzo8677@gmail.com>
2025-03-12 14:11:27 -0500

    actualizacion

--- proyecto/src/Components/pages/ProfessionalExp/ProfessionalExp.jsx
+++ proyecto/src/Components/pages/ProfessionalExp/ProfessionalExp.jsx
@@ -1,10 +1,15 @@
+import { CardInfo } from "../../UI/CardInfo/CardInfo"
+import img1 from "../../assets/image/imgenExpTra.avif"
 
 export const ProfessionalExp = () => {
   return (
     <main className="flex-grow min-h-screen flex flex-col items-center p-4">
-      <h1>experiencia profecional</h1>
-      <p>no cuento con experiencia laboral pero no letengo miedo al trabajo</p>
       
+      <CardInfo
+      title={"experiencia profecional"}
+        content={"no cuento con experiencia laboral pero no le tengo miedo al trabajo"}
+      />      
+      <img src = {img1} alt="imagen 1" />
     </main>
   )
 }
--- proyecto/src/Components/pages/Studies/Studies.jsx
+++ proyecto/src/Components/pages/Studies/Studies.jsx
@@ -1,10 +1,31 @@
-
+import { CardInfo } from "../../UI/CardInfo/CardInfo"
 
 export const Studies = () => {
   return (
 
-    <main className="flex-grow min-h-screen flex flex-col items-center p-4">
-      
+    <main className="flex-grow min-h-screen flex flex-col justify-items-center  p-4">
+      <h1>ruta de estudios</h1>
+      <div className="  grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1  gap-4">
+        <CardInfo
+          title={"inicios de mi aprendisaje"}
+          content={"hila"}
+        />
+
+        <CardInfo
+          title={"inicios de mi aprendisaje"}
+          content={"hila"}
+        />
+
+        <CardInfo
+          title={"inicios de mi aprendisaje"}
+          content={"hila"}
+        />
+
+        <CardInfo
+          title={"inicios de mi aprendisaje"}
+          content={"hila"}
+        />
+      </div>
     </main>
   )
 }
